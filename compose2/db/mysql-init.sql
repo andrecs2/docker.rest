78a57cd5e2be6b4231cecf0f2d43effc14708886
André Carlos <andrecs2@gmail.com>
2018-03-08 23:23:05 -0300

    limpeza final

--- compose2/db/mysql-init.sql
+++
@@ -1,2 +0,0 @@
--- drop database if exists stream;
-create database stream;
--- create_docker_machine.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash  
-for i in 1 2 3; do docker-machine create --driver generic --generic-ip-address=192.168.0.15$i --generic-ssh-key ~/.ssh/id_rsa node-$i; done
-echo "criados"
